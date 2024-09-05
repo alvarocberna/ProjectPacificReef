4b5cdeede4cefd5c674b57f64e516f6bbce48a5f
alvaro canete <cberna.alvaro@gmail.com>
2024-09-05 12:45:08 -0400

    creacion de usuario e inicio de sesion añadido

--- ProyectoPacificReef/settings.py
+++ ProyectoPacificReef/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ROLES = (
+    ('admin', 'Administrador'),
+    ('cliente', 'Cliente'),
+    ('empleado', 'Empleado'),
+)
